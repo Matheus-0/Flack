b96abcd1d267bdfa01dea07b4bdb0622b3f2b181
Dywyll <scriptermatt@gmail.com>
2020-04-26 18:40:40 -0300

    Added models and basic sign up system.

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 
 from forms import *
+from models import *
 
 app = Flask(__name__)
 
 app.secret_key = 'dev'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = None
+
+db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,7 +17,20 @@ def index():
     registration_form = RegistrationForm()
 
     if registration_form.validate_on_submit():
-        return "Success!"
+        username = registration_form.username.data
+        password = registration_form.password.data
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            return 'This username is already being used.'
+
+        user = User(username=username, password=password)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return 'Success!'
 
     return render_template('index.html', form=registration_form)
 
