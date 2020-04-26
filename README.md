4d95248d3058629e20d3b49e1deb625f12126eb2
Dywyll <scriptermatt@gmail.com>
2020-04-26 16:05:15 -0300

    Added details on README.md. Changed route.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Flack
 A web chat app made in Python and JavaScript.
+
+### Language(s)
+* CSS
+* HTML
+* Python
+* JavaScript
+
+### Framework(s)
+* Flask
--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
+app.secret_key = os.environ.get('SECRET_KEY')
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'This is the index page.'
 
 
 if __name__ == '__main__':
